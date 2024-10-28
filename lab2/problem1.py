4fa7290f563e22743ae6e6ec01f36bac8c5ed6f8
Viktor Pazhev <asusvikp@gmail.com>
2024-10-28 16:09:29 +0200

    Add lab 2

---
+++ lab2/problem1.py
@@ -0,0 +1,39 @@
+from collections import namedtuple
+
+Point = namedtuple('Point', ['x', 'y'])
+Rectangle = namedtuple('Rectangle', ['start', 'end', 'area'])
+
+def calculate_area(a, b):
+    width = abs(a.x - b.x)
+    height = abs(a.y - b.y)
+
+    return width * height
+
+def get_areas(starting_points, ending_points, n):
+    return sorted(filter(lambda rect: rect.area > n, \
+                    map(lambda x: Rectangle(x[0], x[1], calculate_area(x[0], x[1])), \
+                        zip(starting_points, ending_points))), key = lambda rect: -rect.area)
+
+starting_points = [
+    Point(2, 3), 
+    Point(0, 0), 
+    Point(3, 4), 
+    Point(5, 6),
+    Point(3, 3)
+]
+ending_points = [
+    Point(3, 4), 
+    Point(-5, -9), 
+    Point(7, 7), 
+    Point(5, 6),
+    Point(0, 0)
+]
+
+# Fix test
+expected_result = [
+    Rectangle(Point(x=0, y=0), Point(x=-5, y=-9), 45), 
+    Rectangle(Point(x=3, y=4), Point(x=7, y=7), 12),
+]
+
+assert get_areas(starting_points, ending_points, 9) == expected_result
+print("All OK! +0.75 point")
